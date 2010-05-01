4a68483074fecd56eb740dd413b8bb39a1f4c72f
David LaPalomento <dml@eta.(none)>
2010-04-30 23:02:21 -0400

    copy over cpp backend to js backend rename all cpp* to js*

---
+++ lib/Target/JsBackend/JsTargetMachine.h
@@ -0,0 +1,44 @@
+//=== JsTargetMachine.h - TargetMachine for the Javscript backend -*- js -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file declares the TargetMachine that is used by the Javascript backend.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef JSTARGETMACHINE_H
+#define JSTARGETMACHINE_H
+
+#include "llvm/Target/TargetMachine.h"
+#include "llvm/Target/TargetData.h"
+
+namespace llvm {
+
+class formatted_raw_ostream;
+
+struct JsTargetMachine : public TargetMachine {
+  JsTargetMachine(const Target &T, const std::string &TT,
+                   const std::string &FS)
+    : TargetMachine(T) {}
+
+  virtual bool WantsWholeFile() const { return true; }
+  virtual bool addPassesToEmitWholeFile(PassManager &PM,
+                                        formatted_raw_ostream &Out,
+                                        CodeGenFileType FileType,
+                                        CodeGenOpt::Level OptLevel,
+                                        bool DisableVerify);
+
+  virtual const TargetData *getTargetData() const { return 0; }
+};
+
+extern Target TheJsBackendTarget;
+
+} // End llvm namespace
+
+
+#endif
---
+++ lib/Target/JsBackend/TargetInfo/JsBackendTargetInfo.cpp
@@ -0,0 +1,26 @@
+//===-- JsBackendTargetInfo.cpp - JsBackend Target Implementation -------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "JsTargetMachine.h"
+#include "llvm/Module.h"
+#include "llvm/Target/TargetRegistry.h"
+using namespace llvm;
+
+Target llvm::TheJsBackendTarget;
+
+static unsigned JsBackend_TripleMatchQuality(const std::string &TT) {
+  // This class always works, but shouldn't be the default in most cases.
+  return 1;
+}
+
+extern "C" void LLVMInitializeJsBackendTargetInfo() { 
+  TargetRegistry::RegisterTarget(TheJsBackendTarget, "js",    
+                                  "Javascript backend",
+                                  &JsBackend_TripleMatchQuality);
+}
